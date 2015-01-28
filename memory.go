cfd9dc7a8d14c8c7f63de3e4e39625bf20e89b59
Rohan Singh <rohan@spotify.com>
2015-01-27 19:48:45 -0500

    Split up JobStore/InMemoryJobStore
    Give the interface its own file.

--- memory.go
+++ memory.go
@@ -7,34 +7,6 @@ import (
 	"sync/atomic"
 )
 
-// JobStore write and reads jobs and job logs from persistent storage.
-type JobStore interface {
-	// GetByID gets a job by its unique ID.
-	GetByID(id uint) (*Job, error)
-
-	// GetByNumber gets a job for the given owner/repo/branch by its number.
-	GetByNumber(owner, repo, branch string, number uint) (*Job, error)
-
-	// Lists gets all the jobs for the given owner/repo/branch.
-	List(owner, repo, branch string) ([]*Job, error)
-
-	// Save persists a job to storage. If the job doesn't have an ID yet, it is assigned a
-	// unique ID and the next incrementing job number for the owner/repo/branch.
-	Save(j *Job) error
-
-	// GetLogger gets the JobLogger to write logs for a job.
-	GetLogger(j *Job) JobLogger
-}
-
-// JobLogger provides an io.Writer interface for writing build logs for a job.
-type JobLogger interface {
-	io.Writer
-
-	// WriteStep writes a transition to a new build step to the log. All subsequent writes are
-	// assumed to be part of the new build step, until another new step is written.
-	WriteStep(name string) error
-}
-
 // InMemoryJobStore is a mock JobStore that only stores jobs in memory and writes logs directly
 // to stdout. It is only meant to be used for testing.
 type InMemoryJobStore struct {
